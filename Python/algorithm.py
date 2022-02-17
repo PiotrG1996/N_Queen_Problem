d832da7253ede83f909aa5c58c21c52599f34f33
Piotr Gapski <admin@Piotrs-MacBook-Pro.local>
2022-02-17 19:25:00 +0100

    generate chessboard in python

--- Python/algorithm.py
+++ Python/algorithm.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as matplot
+import numpy as np
+
+
 def is_valid_state(state):
     # check if it is a valid solution
     return True
@@ -23,3 +27,15 @@ def solve():
     state = set()
     search(state, solutions)
     return solutions
+
+
+board = np.tile(np.eye(2)[0], (8, 4))
+for i in range(board.shape[0]):
+    board[i] = np.roll(board[i], i)
+
+    # Create a random board
+    # board[i] = np.random.permutation(board[i])
+
+matplot.gray()
+matplot.matshow(board)
+matplot.show()
